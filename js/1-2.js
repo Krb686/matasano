63e13ca0554b44b9b9d0f052d52f0e4617381d4c
kevin <krb686@gmail.com>
2015-09-23 15:55:23 -0700

    Finish challenge 1-2

--- js/1-2.js
+++ js/1-2.js
@@ -1,6 +1,25 @@
 module.exports = {
   xorStrings: function(s1, s2){
-    
+
+    var output = "";
+
+    if(s1.length != s2.length){
+      console.log("Both input strings must be the same length!");
+      process.exit();
+    }
+
+    for(var i=0;i<s1.length;i++){
+      c1 = s1[i];
+      c2 = s2[i];
+
+      n1 = parseInt(c1, 16);
+      n2 = parseInt(c2, 16);
+
+      output += (n1^n2).toString();
+
+    }
+
+    return output;
 
   }
 };
