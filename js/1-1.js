ba8da2e5b3b61e5cef148696aa57d63cd7e49ce6
kevin <krb686@gmail.com>
2015-09-23 15:34:04 -0700

    Remove excess junk

--- js/1-1.js
+++ js/1-1.js
@@ -1,5 +1,3 @@
-var string = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
-
 module.exports = {
   convertBase16To64 : function(base16String){
 
