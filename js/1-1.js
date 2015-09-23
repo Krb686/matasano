3c1deacf4b9ad6aff9ce72b5a4c07e3a22cf745b
kevin <krb686@gmail.com>
2015-09-22 18:22:20 -0700

    add main functionality of converting from base 16 to 64. Almost done, just need to reverse string around

--- js/1-1.js
+++ js/1-1.js
@@ -1,5 +1,33 @@
 var string = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
 
+
+
+
+function convertHexTo64(hexString){
+  var i;
+
+  var output = "";
+
+  for(i=hexString.length-3;i>=0;i-=3){
+    subString = hexString.substring(i,i+3);
+
+    index1_16 = BASE_16.indexOf(subString[0]);
+    index2_16 = BASE_16.indexOf(subString[1]);
+    index3_16 = BASE_16.indexOf(subString[2]);
+
+    index1_64 = (index1_16*4) + Math.floor(index2_16 / 4); 
+    index2_64 = ((index2_16 % 4)*16) + index3_16;
+
+    output += BASE_64[index1_64] + BASE_64[index2_64];
+  }
+
+  if(i>0){
+    console.log("not done");
+  }
+
+  return output;
+
+}
 BASE_16	=	[	"0","1","2","3","4","5","6","7",
 			"8","9","a","b","c","d","e","f"
 		];
@@ -16,15 +44,6 @@ BASE_64 = 	[	"A","B","C","D","E","F","G","H",
 		];
 
 
-for(var i=string.length-1;i>=0;i--){
-	hex_digit=string[i];
-	
-
-}
-
-
-		
-		
-
-
+newString = convertHexTo64(string);
+console.log(newString);
 
