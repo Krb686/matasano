22918171a2a38522d224a035558d9661bed8e0bc
kevin <krb686@gmail.com>
2015-09-23 15:39:11 -0700

    Starting challenge 2, make test driver, placeholders

---
+++ js/1-2.js
@@ -0,0 +1,6 @@
+module.exports = {
+  xorStrings: function(s1, s2){
+    
+
+  }
+};
---
+++ js/test_1-2.js
@@ -0,0 +1,5 @@
+var Utility = require("./1-2.js");
+var s1 = "1c0111001f010100061a024b53535009181c";
+var s2 = "686974207468652062756c6c277320657965";
+var output = Utility.xorStrings(s1, s2);
+console.log(output);
