dff4da625480bdfafef5e605c2617a5b7e1af81c
kevin <krb686@gmail.com>
2015-09-26 08:03:14 -0700

    Completely modularize c3.  Create test driver for c3

--- js/set1/c3/c3.js
+++ js/set1/c3/c3.js
@@ -1,6 +1,4 @@
 // Matasano challenge #3
-
-
 module.exports = (function(){
 
     // English language letter frequency * 100
@@ -35,118 +33,62 @@ module.exports = (function(){
         "z":1
     };
 
-    return {
-        scoreText: function(string){
-            var score = 0;
-            var freq = {};
+    function scoreText(string){
+        var score = 0;
+        var freq = {};
+        var MAX_ERROR = 1000;
 
-            // Count occurences of each character
-            for(var i=0;i<string.length;i++){
-                if(!freq.hasOwnProperty(string[i])){
-                    freq[string[i]] = 0;
-                }
-                freq[string[i]]++;
+        // Count occurences of each character
+        for(var i=0;i<string.length;i++){
+            if(!freq.hasOwnProperty(string[i])){
+                freq[string[i]] = 0;
             }
-            freq["total"] = string.length;
-
-
-            // Compute error for each frequency
-            for(var property in freq){
-                if(freq.hasOwnProperty(property)){
-                    console.log("Computing: " + freq[property] + " / " + freq["total"]);
-                    var freqInt = Math.round(freq[property] / freq["total"] * 100);
-                    console.log(freqInt);
-                }
-            }
-
-
-
-            return score;
-
-        },
-
-        findBestScore: function(strings){
-            return 0;
+            freq[string[i]]++;
         }
+        freq["total"] = string.length;
+
+
+        // Compute error for each frequency
+        var totalError = 0;
+        for(var property in freq){
+            if(freq.hasOwnProperty(property)){
+                //console.log("Computing: " + freq[property] + " / " + freq["total"]);
+                if(property != "total"){
+                    var freqInt = Math.round(freq[property] / freq["total"] * 1000);
+                    if(letterFrequency.hasOwnProperty(property)){
+                        var error = Math.abs(letterFrequency[property] - freqInt);
+                    } else {
+                        var error = freqInt;
+                    }
+                    totalError += error;
+                //console.log("Property: " + property);
+                //console.log("Freq: " + freqInt);
+                //console.log("Error: " + error);
+                //console.log("\n");
+                }
+            }
+        }
+
+        return MAX_ERROR - totalError;
     }
+
+    function findBestScore(strings){
+        var highest = 0;
+        var best = "";
+        for(var i=0;i<strings.length;i++){
+            var string = strings[i];
+            var score = scoreText(string);
+            if(score > highest){
+                highest = score;
+                best = string;
+            }
+        }
+        return [best, highest];
+    }
+
+    return {
+        scoreText: scoreText,
+        findBestScore: findBestScore
+    };
 
 })();
-
-var charSet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-var Utility1 = require("../c1/c1.js");
-var Utility2 = require("../c2/c2.js");
-
-var string = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
-
-var letterFreq = "zxqjkvbpygfwmucldrhsnioate";
-
-
-ascii_string = convertHexToAscii(string);
-
-var highest_score =0;
-var winner = "";
-
-var test_cases = {};
-// Loop through each test case
-for(var i=0;i<256;i++){
-  c = String.fromCharCode(i);
-  testString = Array(ascii_string.length + 1).join(c);
-
-  var output = ""
-  var score = 0;
-
-  if(!test_cases.hasOwnProperty(c)){
-    test_cases[c] = {};
-  }
-  var char_freq = {};
-
-  // Loop through each character in the input string
-  for(var j=0;j<ascii_string.length;j++){
-    var numeric_result = ascii_string[j].charCodeAt(0)^c.charCodeAt(0);
-    plain_char = String.fromCharCode(numeric_result);
-    if(!char_freq.hasOwnProperty(plain_char)){
-      char_freq[plain_char] = 0;
-    }
-    char_freq[plain_char]++;
-    score += letterFreq.indexOf(plain_char.toLowerCase());
-    output += plain_char;
-  }
-
-  char_freq["total"] = ascii_string.length;
-  test_cases[c] = char_freq;
-  //console.log(output);
-  //console.log(char_freq);
-  //console.log("\n");
-  //console.log(score);
-  //console.log("\n");
-  if(score > highest_score){
-    highest_score = score;
-    winner = output;
-  }
-}
-//console.log(winner);
-console.log(module.exports);
-module.exports.scoreText(ascii_string);
-
-
-function convertHexToAscii(string){
-  var output = "";
-  for(var i=0;i<string.length;i+=2){
-    hexChar = string.substring(i, i+2);
-    ascii = String.fromCharCode(parseInt(hexChar, 16));
-    output += ascii;
-  }
-
-  return output
-}
-
-function convertAsciiToHex(inputString){
-  var output = "";
-  for(var i=0;i<inputString.length;i++){
-    hex = inputString[i].charCodeAt(0).toString(16);
-    output += hex;
-  }
-
-  return output;
-}
