0ccf6ee502f8213dee63f9f42210c9009d483781
kevin <krb686@gmail.com>
2015-09-27 18:42:38 -0700

    Added ability to convert back from base64 to base16

--- js/set1/c1/Converter.js
+++ js/set1/c1/Converter.js
@@ -1,24 +1,22 @@
 module.exports = (function(){
 
+    var BASE_16 = [ "0","1","2","3","4","5","6","7",
+                    "8","9","a","b","c","d","e","f"
+                  ];
+
+    var BASE_64 =  [ "A","B","C","D","E","F","G","H",
+                     "I","J","K","L","M","N","O","P",
+                     "Q","R","S","T","U","V","W","X",
+                     "Y","Z","a","b","c","d","e","f",
+                     "g","h","i","j","k","l","m","n",
+                     "o","p","q","r","s","t","u","v",
+                     "w","x","y","z","0","1","2","3",
+                     "4","5","6","7","8","9","+","/"
+                   ];
 
     function convertBase16To64(base16String){
 
         var output = "";
-
-        var BASE_16 =  [ "0","1","2","3","4","5","6","7",
-                         "8","9","a","b","c","d","e","f"
-                       ];   
-
-
-        var BASE_64 =  [ "A","B","C","D","E","F","G","H",
-                         "I","J","K","L","M","N","O","P",
-                         "Q","R","S","T","U","V","W","X",
-                         "Y","Z","a","b","c","d","e","f",
-                         "g","h","i","j","k","l","m","n",
-                         "o","p","q","r","s","t","u","v",
-                         "w","x","y","z","0","1","2","3",
-                         "4","5","6","7","8","9","+","/"
-                       ];
 
         // Add padding 0's if necessary to keep length a multiple of 3
         if(base16String.length % 3 != 0){
@@ -44,7 +42,21 @@ module.exports = (function(){
 
     function convertBase64To16(base64String){
 
+        var output = "";
 
+        for(var i=0;i<base64String.length;i+=2){
+            subString = base64String.substring(i, i+2);
+
+            index1_64 = BASE_64.indexOf(subString[0]);
+            index2_64 = BASE_64.indexOf(subString[1]);
+
+            index1_16 = Math.floor(index1_64 / 4);
+            index2_16 = ((index1_64 % 4) * 4) + Math.floor(index2_64 / 16);
+            index3_16 = index2_64 % 16;
+
+            output += BASE_16[index1_16] + BASE_16[index2_16] + BASE_16[index3_16];
+        }
+        return output;
 
     }
 
